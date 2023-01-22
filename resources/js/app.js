8a6431ee6e4f6c49a3635081e934f15a4a5e7a04
Hridoy Roy <hridayroy05@gmail.com>
2023-01-22 23:53:55 +0600

    after reinstall laravel and vue

--- resources/js/app.js
+++
@@ -1,4 +0,0 @@
-import {createApp} from "vue";
-import Welcome from "./welcome.vue";
-
-createApp(Welcome).mount("#app");
---
+++ vue-breeze-api/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Vue Breeze Api</h1>
+</template>
+
